e9b4b8816eb00e20d1a586a6abca941b4b233b0f
Princeeeboyeee <88536083+Princeeeboyeee@users.noreply.github.com>
2022-07-08 23:29:41 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
- 
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(425,400);
+    canvas=createCanvas(425,400);
+    canvas.position(800,250);
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background(255,189,34);
+}
+
+function modelLoaded()
+{
+    console.log( "PoseNet Model Loaded !!!");
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+    } 
+} 
  
